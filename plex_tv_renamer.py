665c4f11cb7f0f42d920b118e385f625ae4d2368
Ezra Werlinich <ewerlinich@gmail.com>
2022-04-23 19:00:41 -0400

    Added more script input options for customizing naming. Also added further documentation and comments for clarity.

--- plex_tv_renamer.py
+++ plex_tv_renamer.py
@@ -1,5 +1,4 @@
 import argparse
-import enum
 import os
 
 def vPrint(message) :
@@ -13,7 +12,7 @@ def vPrint(message) :
         print(message)
 
 
-def renameEpisodes(dir, name, season) :
+def renameSeason(dir, name, season) :
     """
     Renames a season of a TV show following Plex's recommended naming conventions.
 
@@ -31,23 +30,28 @@ def renameEpisodes(dir, name, season) :
     counter = 0
     season_str = str(season)
     
-    for count, f in enumerate(os.listdir()) :
+    for count, f in enumerate(sorted(os.listdir())) :
         f_name, f_ext = os.path.splitext(f)
         count_str = str(count + 1)
+
+        f_name = f'{name}'
+        if not (year is None) :
+            f_name = f'{f_name} ({year})'
+
         if(season > 9) :
             if((count + 1) > 99) :
-                f_name = f'{name} - S{season_str}E{count_str}'
+                f_name = f'{f_name} - S{season_str}E{count_str}'
             elif((count + 1) > 9) :
-                f_name = f'{name} - S{season_str}E0{count_str}'
+                f_name = f'{f_name} - S{season_str}E0{count_str}'
             else :
-                f_name = f'{name} - S{season_str}E00{count_str}'
+                f_name = f'{f_name} - S{season_str}E00{count_str}'
         else :
             if((count + 1) > 99) :
-                f_name = f'{name} - S0{season_str}E{count_str}'
+                f_name = f'{f_name} - S0{season_str}E{count_str}'
             if((count + 1) > 9) :
-                f_name = f'{name} - S0{season_str}E0{count_str}'
+                f_name = f'{f_name} - S0{season_str}E0{count_str}'
             else :
-                f_name = f'{name} - S0{season_str}E00{count_str}'
+                f_name = f'{f_name} - S0{season_str}E00{count_str}'
                 
             new_name = f'{f_name}{f_ext}'
             os.rename(f, new_name)
@@ -55,28 +59,40 @@ def renameEpisodes(dir, name, season) :
             
     return counter
     
-def renameSeason(dir, name) :
+def renameShow(dir, name) :
     """
-    Rename a set of seasons for a particular TV show, including all of the episodes
-    within their respective seasons.
+    Rename every season for a particular TV show at once, including all of the
+    episodes within their respective seasons.
 
     Args:
         dir (str): The file directory that the seasons are contained in.
         name (str): The name of the TV show.
+
+    Returns:
+        tuple (int, int): The total amount of seasons and episodes over the whole show.
     """
-    vPrint(f'Renaming a set of seasons.')
+    vPrint(f'Renaming a full show.')
     os.chdir(dir)
     vPrint(f'Changing directory to {directory}')
+
     ep_num_one = 1
     ep_num_two = 0
-    for count, f in enumerate(os.listdir()) :
+    count_str = 0
+
+    # f is the current file/folder name
+    for count, f in enumerate(sorted(os.listdir())) :
         count_str = str(count + 1)
-        if(f == "Specials" or f == "specials") :
+        if(f == "Specials" or f == "specials") :  
             print("Skipping Specials folder.")
             count -= 1
             continue
-        new_dir = directory + "\\" + f
-        ep_num_two += renameEpisodes(new_dir, show_name, count + 1)
+
+        if(opsys == 'nt') :
+            new_dir = directory + "\\" + f
+        else :
+            new_dir = directory + "/" + f
+
+        ep_num_two += renameSeason(new_dir, show_name, count + 1)
         vPrint(f'Finished renaming current season.')
         ep_num_one_str = str(ep_num_one)
         ep_num_two_str = str(ep_num_two)
@@ -85,48 +101,63 @@ def renameSeason(dir, name) :
         
         new_name = ""
         if((count + 1) > 9) :
-            new_name = f'{name} - Season {count_str} (Episodes '
+            new_name = f'Season {count_str}'
         else :
-            new_name = f'{name} - Season 0{count_str} (Episodes '
-            
-        if(ep_num_one > 99) :
-            new_name = f'{new_name}{ep_num_one_str}-'
-        elif(ep_num_one > 9) :
-            new_name = f'{new_name}0{ep_num_one_str}-'
-        else :
-            new_name = f'{new_name}00{ep_num_one_str}-'
-            
-        if(ep_num_two > 99) :
-            new_name = f'{new_name}{ep_num_two_str})'
-        elif(ep_num_two > 9) :
-            new_name = f'{new_name}0{ep_num_two_str})'
-        else :
-            new_name = f'{new_name}00{ep_num_two_str})'
+            new_name = f'Season 0{count_str}'
+
+        if(absolute) :
+            new_name = f'{new_name} (Episodes '
+            if(ep_num_one > 99) :
+                new_name = f'{new_name}{ep_num_one_str}-'
+            elif(ep_num_one > 9) :
+                new_name = f'{new_name}0{ep_num_one_str}-'
+            else :
+                new_name = f'{new_name}00{ep_num_one_str}-'
+                
+            if(ep_num_two > 99) :
+                new_name = f'{new_name}{ep_num_two_str})'
+            elif(ep_num_two > 9) :
+                new_name = f'{new_name}0{ep_num_two_str})'
+            else :
+                new_name = f'{new_name}00{ep_num_two_str})'
+
         os.rename(f, new_name)
         vPrint(f'Changing {f} to {new_name}')
         
         ep_num_one = ep_num_two + 1
 
+    return (count_str, ep_num_two)
+
+# 'nt' if Windows, 'posix' if Linux or OSX. Needed to determine
+# forward vs backward slashes in directory names
+opsys = os.name
+
 all_args = argparse.ArgumentParser()
 all_args.add_argument("directory", help="The directory containing the seasons/episodes to be renamed")
 all_args.add_argument("name", help="The name of the TV show")
-all_args.add_argument("season", type=int, help="The season number. Pass 0 to rename a collection of seasons instead of a single one")
+all_args.add_argument("season", type=int, help="The season number. Pass 0 to rename a full series instead of a single season")
 all_args.add_argument("-v", "--verbose", help="Use this to display more information", action="store_true")
+all_args.add_argument("-a", "--absolute", help="Add absolute (cumulative) episode counts in the folder name for the seasons", action="store_true")
+all_args.add_argument("-y", "--year", type=int, help="Include the given year in the folder names for the seasons")
 args = vars(all_args.parse_args())
 
-verbose = args["verbose"]
-directory = args["directory"]
-show_name = args["name"]
-season_num = args["season"]
+verbose =       args["verbose"]
+directory =     args["directory"]
+show_name =     args["name"]
+season_num =    args["season"]
+absolute =      args["absolute"]
+year =          args["year"]
 vPrint(f'Program set to verbose mode.')
 vPrint(f'Directory: {directory}')
 vPrint(f'Name: {show_name}')
+vPrint(f'Release year: {year}')
 vPrint(f'Season number: {season_num}')
+vPrint(f'Absolute numbering: {absolute}')
 
+counts = (0, 0)
 if(season_num == 0) :
-    renameSeason(directory, show_name)
-    print(f'All done!')
+    counts = renameShow(directory, show_name)
 else :
-    renameEpisodes(directory, show_name, season_num)
-    print(f'All done!')
-
+    epcount = renameSeason(directory, show_name, season_num)
+    counts = (1, epcount)
+print(f'Done. Renamed {str(counts[0])} seasons containing {str(counts[1])} episodes.')
